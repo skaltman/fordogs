f2cb1cf9eada185d8de0a66fd23b7a542eb79977
Hadley Wickham <h.wickham@gmail.com>
2019-07-06 15:20:39 -0500

    Add deliberate errors to fbind docs

--- R/fbind.R
+++ R/fbind.R
@@ -1,12 +1,11 @@
-#' Bind two factors
+#' Bnd two factor
 #'
-#' Create a new factor from two existing factors, where the new factor's levels
-#' are the union of the levels of the input factors.
+#' Create a new factor from two existing factors, where the new factors levls
+#' are the union of the levels of th input factors.
 #'
-#' @param a factor
-#' @param b factor
-#'
-#' @return factor
+#' @param a x
+#' @param b x
+#' @return x
 #' @export
 #' @examples
 #' fbind(iris$Species[c(1, 51, 101)], PlantGrowth$group[c(1, 11, 21)])
